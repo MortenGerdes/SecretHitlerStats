b800c9e2d76e7fdef2f6abb28163ce66246125ec
Morten <mortengerdes7000@gmail.com>
2018-03-17 03:07:34 +0100

    Testing adding stats

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -51,7 +52,46 @@ public class Main
 
     public void registerPostRoutes()
     {
+        post("/registerstats", (req, res) ->
+        {
+            String steamID, hostSteamID, roleType, didWin, theDate, amountOfPlayers, withAbilities, amountLibsPlayed, amountFascPlayed;
 
+            steamID = req.queryParams("SteamID");
+            hostSteamID = req.queryParams("HostSteamID");
+            roleType = req.queryParams("RoleType");
+            didWin = req.queryParams("DidWin");
+            theDate = req.queryParams("TheDate");
+            amountOfPlayers = req.queryParams("AmountOfPlayers");
+            withAbilities = req.queryParams("WithAbilities");
+            amountLibsPlayed = req.queryParams("AmountLibsPlayed");
+            amountFascPlayed = req.queryParams("AmountLibsPlayed");
+
+            if(steamID == null || hostSteamID == null || roleType == null || didWin == null)
+            {
+                res.status(400);
+                return res;
+            }
+
+            if(Integer.parseInt(roleType) > 1)
+            {
+                res.status(400);
+                res.body("<p>Invalid roleType. 0 = lib, 1 = fasc</p>");
+            }
+
+            if(insertStat(
+                    Long.parseLong(steamID), Long.parseLong(hostSteamID), Integer.parseInt(roleType),
+                    Boolean.parseBoolean(didWin), Date.valueOf(theDate), Integer.parseInt(amountOfPlayers),
+                    Boolean.parseBoolean(withAbilities), Integer.parseInt(amountLibsPlayed), Integer.parseInt(amountFascPlayed)))
+            {
+                res.status(200);
+            }
+            else
+            {
+                res.status(400);
+                res.body("<p> Something went wrong when trying to add valeus into database. <br> make sure you parsed the values correctly</p>");
+            }
+            return res;
+        });
     }
 
     public void registerPutRoutes()
@@ -114,6 +154,31 @@ public class Main
             PreparedStatement ps = conn.prepareStatement(query);
             ps.setLong(1, steamID);
             ps.setString(2, name);
+            rowsAffected = ps.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (rowsAffected == 1);
+    }
+
+    private boolean insertStat(long steamID, long hostSteamID, int roleType, boolean didWin, Date theDate, int amountOfPlayers, boolean withAbilitites, int amountLibsPlayed, int amountFascPlayed)
+    {
+        int rowsAffected = 0;
+        String query = "INSERT INTO Games (SteamID, HostSteamID, RoleType, DidWin, TheDate, AmountOfPlayers, WithAbilities, AmountLibsPlayed, AmountFascPlayed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            Connection conn = connectionPool.getConnection();
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setLong(1, steamID);
+            ps.setLong(2, hostSteamID);
+            ps.setInt(3, roleType);
+            ps.setBoolean(4, didWin);
+            ps.setDate(5, theDate);
+            ps.setInt(6, amountOfPlayers);
+            ps.setBoolean(7, withAbilitites);
+            ps.setInt(8, amountLibsPlayed);
+            ps.setInt(9, amountFascPlayed);
+
             rowsAffected = ps.executeUpdate();
             conn.close();
         } catch (SQLException e) {
